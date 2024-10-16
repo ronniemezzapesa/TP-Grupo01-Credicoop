9112b62695c720014e3279716343a5af00ab00fe
ronniemezzapesa <rmezzapesa@frba.utn.edu.ar>
2024-10-16 16:28:45 -0300

    Borro configuraciones

---
+++ docker-produccion/LocalSettings.php
@@ -0,0 +1,10 @@
+<?php
+$wgSitename = "miwiki";
+$wgMetaNamespace = "Miwiki";
+
+$wgServer = "http://192.168.232.133:7000"; 
+$wgDBtype = "mysql";
+$wgDBserver = "db"; 
+$wgDBname = "wikidb"; 
+$wgDBuser = "wikiuser"; 
+$wgDBpassword = "wikisecret"; 
